abb4e0a6546585fdd524f5ce9a74e8044ac0a3d0
NaikawadiSagar <sagarnaikawadi2605@gmail.com>
2023-04-24 21:50:22 +0630

    2nd Assignment

---
+++ 02_Assignment/01 Accept One Number From User And Print That Number Star on Print.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<conio.h>
+
+void Display(int iNo)
+{
+    int iCnt = 0;
+
+    while(iCnt < iNo)
+    {
+        printf(" * ");
+
+        iCnt++;
+    }
+}
+
+int main()
+{
+    int iValue = 0;
+
+    printf("\n Enter A Value => ");
+    scanf("%d",&iValue);
+
+    Display(iValue);
+
+    getch();
+    return 0;
+}
