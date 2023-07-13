7e659f36e4682fa6552d5bf2d9c5fe12b9efef24
NaikawadiSagar <sagarnaikawadi2605@gmail.com>
2023-07-13 14:15:20 +0630

    5th Assignemt

---
+++ 05_Assignment/01 Write Program Which Accept Number From User And Display its Multiplication of Factors.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include<conio.h>
+
+int MulFact(int iNo)
+{
+    int i = 0,Cal = 1;
+
+    for(i = 1;i < iNo;i++)
+    {
+        if(iNo % i == 0)
+        {
+            Cal = Cal * i;
+        }
+    }
+
+    return Cal;
+}
+
+int main()
+{
+    int iValue = 0,iRet = 0;
+
+    printf("\n Enter A Number => ");
+    scanf("%d",&iValue);
+
+    iRet = MulFact(iValue);
+
+    printf("\n %d",iRet);
+
+    getch();
+    return 0;
+}
