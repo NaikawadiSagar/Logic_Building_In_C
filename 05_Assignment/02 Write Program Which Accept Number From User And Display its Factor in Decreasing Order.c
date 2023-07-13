28e99cffa61776e81e4c7403891fc97ba51d6bfc
NaikawadiSagar <sagarnaikawadi2605@gmail.com>
2023-07-13 14:16:02 +0630

    Practice

---
+++ 05_Assignment/02 Write Program Which Accept Number From User And Display its Factor in Decreasing Order.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<conio.h>
+
+void MultFact(int iNo)
+{
+    int i = 0;
+
+    for(i = iNo - 1; i >= 1; i--)
+    {
+        if(iNo % i == 0)
+        {
+            printf(" %d",i);
+        }
+    }
+    return;
+}
+
+int main()
+{
+    int iValue = 0;
+    int iRet = 0;
+
+    printf("\n Enter Number => ");
+    scanf("%d",&iValue);
+
+    MultFact(iValue);
+
+    getch();
+    return 0;
+}
