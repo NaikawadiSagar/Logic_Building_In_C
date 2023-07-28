8a068ebf9340353f0b873a81ad5052612bd5038b
NaikawadiSagar <sagarnaikawadi2605@gmail.com>
2023-07-28 14:05:36 +0630

    Practice

---
+++ 06_Assignment/01 Write A Program Which Accept Name From User And Print That Name.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    char Name[30];
+
+    printf("\n Please Enter Full Name => ");
+    scanf("%[^\n]",&Name);
+
+    printf("\n Your Name is %s",Name);
+
+    getch();
+    return 0;
+}
