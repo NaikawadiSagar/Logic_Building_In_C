526937671d078092b57e251e7ecb64b9890856ea
NaikawadiSagar <sagarnaikawadi2605@gmail.com>
2023-07-30 13:54:51 +0630

    practice

---
+++ 06_Assignment/02 Write A Program Which Accept The Number From User And Check Whether That Number Is Greater Than 100 or Not.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+#include<conio.h>
+
+typedef int BOOL;
+
+#define TRUE 1
+#define FALSE 0
+
+BOOL ChkGreater(int iNum)
+{
+    if(iNum > 100)
+    {
+        return 1;
+    }
+
+    return 0;
+}
+
+int main()
+{
+    int iValue = 0;
+
+    BOOL bRet = FALSE;
+
+    printf("\n Please Enter A Number => ");
+    scanf("%d",&iValue);
+
+    bRet = ChkGreater(iValue);
+
+    if(bRet == 1)
+    {
+        printf("\n Number Is Greater");
+    }
+
+    else
+    {
+        printf("\n Number Is Smaller ");
+    }
+
+    getch();
+    return 0;
+}
