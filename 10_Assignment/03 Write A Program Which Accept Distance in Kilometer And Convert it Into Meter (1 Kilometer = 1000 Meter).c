6be255b0b809dd6b6883efcd31714059e22a1ec7
NaikawadiSagar <sagarnaikawadi2605@gmail.com>
2023-08-18 17:46:37 +0630

    Practice

---
+++ 10_Assignment/03 Write A Program Which Accept Distance in Kilometer And Convert it Into Meter (1 Kilometer = 1000 Meter).c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+
+int KMtoMeter(int iNo)
+{
+    return iNo * 1000;
+}
+int main()
+{
+    int iValue = 0,iRet = 0;
+
+    printf("\n Enter A Distance => ");
+    scanf("%d",&iValue);
+
+    iRet = KMtoMeter(iValue);
+
+    printf("\n Distance is %d.",iRet);
+
+    getch();
+    return 0;
+}
