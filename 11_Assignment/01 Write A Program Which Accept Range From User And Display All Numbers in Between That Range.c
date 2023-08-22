d8211943bc6c37aac06bd9a2a3835aac6b457b44
NaikawadiSagar <sagarnaikawadi2605@gmail.com>
2023-08-22 22:11:18 +0630

    Practice

---
+++ 11_Assignment/01 Write A Program Which Accept Range From User And Display All Numbers in Between That Range.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<conio.h>
+
+void RangeDisplay(int iStart,int iEnd)
+{
+    int i = 0;
+
+    if(iStart > iEnd)
+    {
+        printf("\n Invalid Range");
+    }
+
+    for(i = iStart; i <= iEnd; i++)
+    {
+        printf("%3d",i);
+    }
+    return;
+}
+
+int main()
+{
+    int iValue1 = 0,iValue2 = 0;
+
+    printf("\n Enter Starting Point => ");
+    scanf("%d",&iValue1);
+
+    printf("\n Enter Ending Point => ");
+    scanf("%d",&iValue2);
+
+    RangeDisplay(iValue1,iValue2);
+
+    getch();
+    return 0;
+}
