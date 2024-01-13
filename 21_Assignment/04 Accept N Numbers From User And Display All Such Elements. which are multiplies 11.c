319fd5a96f7e702d341aa383ed76df0c5fb65c50
NaikawadiSagar <sagarnaikawadi2605@gmail.com>
2024-01-13 23:52:51 +0530

    Practice

---
+++ 21_Assignment/04 Accept N Numbers From User And Display All Such Elements. which are multiplies 11.c
@@ -0,0 +1,51 @@
+
+#include<stdio.h>
+#include<conio.h>
+#include<stdlib.h>
+
+void Display(int Arr[],int iLength)
+{
+    for(int i = 0;i < iLength; i++)
+    {
+        printf("\n Elemnts is %d",Arr[i]);
+    }
+    for(int i = 0;i < iLength; i++)
+    {
+        if(Arr[i] % 11 == 0 )
+        {
+            printf("\n %d is Divisible By 11",Arr[i]);
+        }
+    }
+    return;
+}
+int main()
+{
+    int iCnt = 0,iSize = 0;
+    int *P = NULL;
+
+    printf("\n Enter Size Of Elements => ");
+    scanf("%d",&iSize);
+
+    P = (int *)malloc(iSize * sizeof(int));
+
+    printf("\n Enter %d Elements = ",iSize);
+
+    if(P == NULL)
+    {
+        printf("\n Unable To Allocated Memory");
+        return -1;
+    }
+
+    for(iCnt = 0;iCnt < iSize;iCnt++)
+    {
+        printf("\n Enter Elements %d : ",iCnt + 1);
+        scanf("%d",&P[iCnt]);
+    }
+    Display(P,iSize);
+    free(P);
+
+    getch();
+    return 0;
+
+}
+
