ee5018fe0ca8240726727b7c13924758d6134e5a
NaikawadiSagar <sagarnaikawadi2605@gmail.com>
2024-02-01 22:48:32 +0530

    Practice

---
+++ 25_Assignment/01 Acccept Character From User And Check whether it is alphabet or not.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<conio.h>
+
+#define TRUE 1
+#define FALSE 0
+typedef int BOOL;
+
+BOOL ChkAlpha(char ch)
+{
+    if((ch >= 65 && ch <= 90) || (ch >= 97 && ch <= 122))
+    {
+        return TRUE;
+    }
+}
+int main()
+{
+    char cValue = '\0';
+    BOOL bRet = FALSE;
+
+    printf("\n Enter The Character => ");
+    scanf("%c",&cValue);
+
+    bRet = ChkAlpha(cValue);
+
+    if(bRet == TRUE)
+    {
+        printf("\n It is Character");
+    }
+    else
+    {
+        printf("\n It is Not a Character");
+    }
+    return 0;
+}
