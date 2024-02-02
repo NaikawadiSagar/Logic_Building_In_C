8fe481580d7e30019f075b1c4382a6378304bd59
NaikawadiSagar <sagarnaikawadi2605@gmail.com>
2024-02-02 22:43:11 +0530

    Practice

---
+++ 25_Assignment/02 Acccept Character From User And Check whether it is Capital or not.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+#include<conio.h>
+
+#define TRUE 1
+#define FALSE 0
+typedef int BOOL;
+
+BOOL ChkCapital(char ch)
+{
+    if(ch >= 65 && ch <= 90)
+    {
+        return TRUE;
+    }
+}
+int main()
+{
+    char cValue = '\0';
+    BOOL bRet = FALSE;
+
+    printf("\n Enter The Character => ");
+    scanf("%c",&cValue);
+
+    bRet = ChkCapital(cValue);
+
+    if(bRet == TRUE)
+    {
+        printf("\n It is Capital Character");
+    }
+    else
+    {
+        printf("\n It is Not a Capital Character");
+    }
+    return 0;
+}
+
