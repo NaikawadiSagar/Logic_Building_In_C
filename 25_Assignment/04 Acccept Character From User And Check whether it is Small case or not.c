63176032a6e42566312dd20e781a16d78b97914d
NaikawadiSagar <sagarnaikawadi2605@gmail.com>
2024-02-03 21:48:00 +0530

    Practice

---
+++ 25_Assignment/04 Acccept Character From User And Check whether it is Small case or not.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<conio.h>
+
+#define TRUE 1
+#define FALSE 0
+typedef int BOOL;
+
+BOOL ChkSmall(char ch)
+{
+    if(ch >= 97 && ch <= 122)
+        return TRUE;
+
+}
+int main()
+{
+    char cValue = '\0';
+    BOOL bRet = FALSE;
+
+    printf("\n Enter The Character => ");
+    scanf("%c",&cValue);
+
+    bRet = ChkSmall(cValue);
+
+    if(bRet == TRUE)
+    {
+        printf("\n It is Small Case Character");
+    }
+    else
+    {
+        printf("\n It is Not Small Case Character ");
+    }
+    return 0;
+}
+
