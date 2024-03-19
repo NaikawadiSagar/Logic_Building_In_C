bf4b545fe90999586dc19e83bb7d9159577ab68c
NaikawadiSagar <sagarnaikawadi2605@gmail.com>
2024-03-19 23:29:28 +0530

    Practice

--- 06_Assignment/04 Write A Program Which Accept Three Numbers And Print its Multiplication.c
+++ 06_Assignment/04 Write A Program Which Accept Three Numbers And Print its Multiplication.c
@@ -34,7 +34,7 @@ int main()
 {
     int iValue1 = 0,iValue2 = 0,iValue3 = 0,iRet = 0;
 
-    printf("\n Enter Three Numbers => ");
+    printf("\n Please Enter Three Numbers => ");
     scanf("%d%d%d",&iValue1,&iValue2,&iValue3);
 
     iRet = Multiply(iValue1,iValue2,iValue3);
